9df785b8348976ad1976d9fd9dcc4ed502e44d43
hoshi53402(ubuntu) <hoshi53402@gmail.com>
2023-03-28 16:59:56 +0900

    - resolved conflict

--- 10_infra/main.tf
+++ 10_infra/main.tf
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # ---------------------------------------------
 # Terraform configuration
 # ---------------------------------------------
@@ -31,37 +30,3 @@ provider "aws" {
   profile = "terraform"
   region  = "us-east-1"
 }
-=======
-# ---------------------------------------------
-# Terraform configuration
-# ---------------------------------------------
-terraform {
-  required_version = ">=0.13"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-  backend "s3" {
-    bucket  = ""
-    key     = "{YOUR-S3-BUCKET-KEY}"
-    region  = "ap-northeast-1"
-    profile = "terraform"
-  }
-}
-
-# ---------------------------------------------
-# Provider
-# ---------------------------------------------
-provider "aws" {
-  profile = "terraform"
-  region  = "ap-northeast-1"
-}
-
-provider "aws" {
-  alias   = "virginia"
-  profile = "terraform"
-  region  = "us-east-1"
-}
->>>>>>> 84bc52ac45e118c8d5227e07e370046740f5bbb3
